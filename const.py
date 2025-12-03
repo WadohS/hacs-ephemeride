569bf0139efa580fd5bdeccc927ee45f79da195e
WadohS <112435749+WadohS@users.noreply.github.com>
2025-12-03 11:00:15 +0100

    Delete const.py

--- const.py
+++
@@ -1,6 +0,0 @@
-"""Constantes pour l'intégration Éphéméride."""
-
-DOMAIN = "ephemeride"
-CONF_LANGUAGE = "language"
-SUPPORTED_LANGUAGES = ["fr", "en", "de", "es", "it", "pt"]
-SENSOR_NAME = "saint_du_jour"
